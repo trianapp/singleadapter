809eb83d2fc0fc09368e6bfa145c543601609a47
triandamai <triannurizkillah@gmail.com>
2021-02-08 12:44:03 +0700

    [UPDATE] update DIff Implementation

--- singleadapter/src/main/java/com/trian/singleadapter/TestActivity.java
+++ singleadapter/src/main/java/com/trian/singleadapter/TestActivity.java
@@ -1,9 +1,9 @@
 package com.trian.singleadapter;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
 
 public class TestActivity extends AppCompatActivity {
     private RecyclerView rv;
@@ -15,11 +15,11 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         rv = findViewById(R.id.rv);
-        adapter = new SingleAdapter<>(R.layout.itemtest, onEventClick)
-                .withRecyclerView(rv);
+        adapter = new SingleAdapter<>(R.layout.itemtest, onEventClick);
 
 
     }
+
     private onEventClick<String> onEventClick = new onEventClick<String>() {
         @Override
         public void onClick(onEventType eventType, String payload, int position) {
